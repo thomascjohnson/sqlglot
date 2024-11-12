47f5cb2de43b653ccef76eeb6b3c898cbf28b202
Thomas Johnson <thomascjohnson@gmail.com>
2024-11-12 16:25:06 +0100

    Simplifies overridden method

--- sqlglot/dialects/teradata.py
+++ sqlglot/dialects/teradata.py
@@ -279,38 +279,14 @@ class Teradata(Dialect):
             return this
 
         def _parse_bitwise(self) -> t.Optional[exp.Expression]:
-            this = self._parse_term()
+            this = super()._parse_bitwise()
 
-            while True:
-                if self._match_set(self.BITWISE):
-                    this = self.expression(
-                        self.BITWISE[self._prev.token_type],
-                        this=this,
-                        expression=self._parse_term(),
-                    )
-                elif self.dialect.DPIPE_IS_STRING_CONCAT and self._match(TokenType.DPIPE):
-                    this = self.expression(
-                        exp.DPipe,
-                        this=this,
-                        expression=self._parse_term(),
-                        safe=not self.dialect.STRICT_STRING_CONCAT,
-                    )
-                elif self._match(TokenType.DQMARK):
-                    this = self.expression(
-                        exp.Coalesce, this=this, expressions=ensure_list(self._parse_term())
-                    )
-                elif self._match_pair(TokenType.LT, TokenType.LT):
-                    this = self.expression(
-                        exp.BitwiseLeftShift, this=this, expression=self._parse_term()
-                    )
-                elif self._match_pair(TokenType.GT, TokenType.GT):
-                    this = self.expression(
-                        exp.BitwiseRightShift, this=this, expression=self._parse_term()
-                    )
-                elif self._match(TokenType.L_PAREN):
-                    this = self._parse_conversion(this)
-                else:
-                    break
+            if this:
+                while True:
+                    if self._match(TokenType.L_PAREN):
+                        this = self._parse_conversion(this)
+                    else:
+                        break
 
             return this
 
